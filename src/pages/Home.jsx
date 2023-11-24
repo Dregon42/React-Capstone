984af73218f494bca9aee7e136009846cc8aec18
Dregon42 <lbrantl13@gmail.com>
2023-11-24 10:18:25 -0500

    hide invite

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import Background from '../assets/stephen-frank-1nls6y6XZJ4-unsplash.jpg';
 import Itinerary from '../components/Itinerary';
 import Profile from '../auth0/Profile';
+import { useAuth0 } from '@auth0/auth0-react';
 
 
 export default function Home() {
+  const { isAuthenticated } = useAuth0();
+
   return (
     <>
         <div className='bg-cover bg-center w-screen h-screen ml-44 flex justify-center p-20 shadow-inner'
           style={{ backgroundImage: `url(${Background})` }}>
             <div className='w-3/4'>
-                <Itinerary />
+              { isAuthenticated && <Itinerary /> }
+                
             </div>
             <Profile />
         </div>
